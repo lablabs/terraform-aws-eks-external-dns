032029fee9569784cbe5c6212ecf63484528e658
Martin Hauskrecht <hauskrechtmartin@gmail.com>
2021-10-05 17:54:13 +0200

    Add dummy helm chart deployment option

--- argo.tf
+++ argo.tf
@@ -1,5 +1,48 @@
-resource "kubernetes_manifest" "this" {
-  count = var.argo_application_enabled ? 1 : 0
+locals {
+  argo_application_values = {
+    "project" : var.argo_project
+    "source" : {
+      "repoURL" : var.helm_repo_url
+      "chart" : var.helm_chart_name
+      "targetRevision" : var.helm_chart_version
+      "helm" : {
+        "releaseName" : var.helm_release_name
+        "parameters" : [for k, v in var.settings : tomap({ "forceString" : true, "name" : k, "value" : v })]
+        "values" : var.values
+      }
+    }
+    "destination" : {
+      "server" : var.argo_destionation_server
+      "namespace" : var.k8s_namespace
+    }
+    "syncPolicy" : var.argo_sync_policy
+    "info" : var.argo_info
+  }
+}
+
+data "utils_deep_merge_yaml" "argo_application_values" {
+  count = var.enabled && var.argo_application_enabled && var.argo_application_use_helm ? 1 : 0
+  input = compact([
+    yamlencode(local.argo_application_values),
+    var.argo_application_values
+  ])
+}
+
+resource "helm_release" "argocd_application" {
+  count = var.enabled && var.argo_application_enabled && var.argo_application_use_helm ? 1 : 0
+
+  chart     = "${path.module}/helm/argocd-application"
+  name      = var.helm_release_name
+  namespace = var.argo_namespace
+
+  values = [
+    data.utils_deep_merge_yaml.argo_application_values[0].output
+  ]
+}
+
+
+resource "kubernetes_manifest" "self" {
+  count = var.enabled && var.argo_application_enabled && !var.argo_application_use_helm ? 1 : 0
   manifest = {
     "apiVersion" = "argoproj.io/v1alpha1"
     "kind"       = "Application"
@@ -7,24 +50,6 @@ resource "kubernetes_manifest" "this" {
       "name"      = var.helm_release_name
       "namespace" = var.argo_namespace
     }
-    "spec" = {
-      "project" = var.argo_project
-      "source" = {
-        "repoURL"        = var.helm_repo_url
-        "chart"          = var.helm_chart_name
-        "targetRevision" = var.helm_chart_version
-        "helm" = {
-          "releaseName" = var.helm_release_name
-          "parameters"  = [for k, v in var.settings : tomap({ "forceString" : true, "name" : k, "value" : v })]
-          "values"      = data.utils_deep_merge_yaml.values[0].output
-        }
-      }
-      "destination" = {
-        "server"    = var.argo_destionation_server
-        "namespace" = var.k8s_namespace
-      }
-      "syncPolicy" = var.argo_sync_policy
-      "info"       = var.argo_info
-    }
+    "spec" = local.argo_application_values
   }
 }
--- variables.tf
+++ variables.tf
@@ -113,6 +113,17 @@ variable "argo_application_enabled" {
   description = "If set to true, the module will be deployed as ArgoCD application, otherwise it will be deployed as a Helm release"
 }
 
+variable "argo_application_use_helm" {
+  type        = bool
+  default     = false
+  description = "If set to true, the ArgoCD Application manifest will be deployed using Kubernetes provider as a Helm release. Otherwise it'll be deployed as a Kubernetes manifest. See Readme for more info"
+}
+
+variable "argo_application_values" {
+  default     = ""
+  description = "Value overrides to use when deploying argo application object with helm"
+}
+
 variable "argo_destionation_server" {
   type        = string
   default     = "https://kubernetes.default.svc"
