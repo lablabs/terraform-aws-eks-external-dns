417c53e36afa74b3ada11a36d9d67c44e2cc42c4
Attila Sovak <attila.sovak@gmail.com>
2025-05-02 14:57:12 +0200

    feat(external-dns): add to migrations support for helm and manifest

--- migrations.tf
+++ migrations.tf
@@ -1,9 +1,4 @@
 moved {
-  from = helm_release.argo_application[0]
-  to   = module.addon.helm_release.argo_application[0]
-}
-
-moved {
   from = aws_iam_role.this[0]
   to   = module.addon-irsa["external-dns"].aws_iam_role.this[0]
 }
@@ -16,4 +11,19 @@ moved {
 moved {
   from = aws_iam_role_policy_attachment.this[0]
   to   = module.addon-irsa["external-dns"].aws_iam_role_policy_attachment.this[0]
+}
+
+moved {
+  from = kubernetes_manifest.this[0]
+  to   = module.addon.kubernetes_manifest.this[0]
+}
+
+moved {
+  from = helm_release.this[0]
+  to   = module.addon.helm_release.this[0]
+}
+
+moved {
+  from = helm_release.argo_application[0]
+  to   = module.addon.helm_release.argo_application[0]
 }
