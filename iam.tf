a2b1066f55bfcebc5e029fd14d80b7147e36a648
Attila Sovak <attila.sovak@gmail.com>
2025-05-05 15:40:49 +0200

    feat(external-dns): fix validation in iam policy

--- iam.tf
+++ iam.tf
@@ -1,8 +1,5 @@
 locals {
-  rbac_create            = var.rbac_create != null ? var.rbac_create : true
-  service_account_create = var.service_account_create != null ? var.service_account_create : true
-  irsa_role_create       = var.irsa_role_create != null ? var.irsa_role_create : true
-  irsa_policy_enabled    = var.enabled && local.rbac_create && local.service_account_create && local.irsa_role_create == false
+  irsa_policy_enabled = var.irsa_policy_enabled != null ? var.irsa_policy_enabled : var.irsa_assume_role_enabled == false
 }
 
 data "aws_iam_policy_document" "this" {
