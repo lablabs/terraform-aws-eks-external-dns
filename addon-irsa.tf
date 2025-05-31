ed4b79fdc453094d6f776bb471a31e635ff5fa7d
atyu1 <github-action@actions-template-sync.noreply.github.com>
2025-05-31 00:28:31 +0000

    feat(sync): sync universal-addon changes
    Signed-off-by: atyu1 <github-action@actions-template-sync.noreply.github.com>

--- addon-irsa.tf
+++ addon-irsa.tf
@@ -2,13 +2,13 @@
 module "addon-irsa" {
   for_each = local.addon_irsa
 
-  source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon-irsa?ref=v0.0.18"
+  source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon-irsa?ref=v0.0.21"
 
   enabled = var.enabled
 
   rbac_create               = var.rbac_create != null ? var.rbac_create : try(each.value.rbac_create, null)
   service_account_create    = var.service_account_create != null ? var.service_account_create : try(each.value.service_account_create, null)
-  service_account_name      = var.service_account_name != null ? var.service_account_name : try(each.value.service_account_name, each.key)
+  service_account_name      = var.service_account_name != null ? var.service_account_name : try(each.value.service_account_name, local.addon_name)
   service_account_namespace = var.service_account_namespace != null ? var.service_account_namespace : try(each.value.service_account_namespace, local.addon_namespace)
 
   # IRSA
@@ -16,7 +16,7 @@ module "addon-irsa" {
   cluster_identity_oidc_issuer_arn = var.cluster_identity_oidc_issuer_arn != null ? var.cluster_identity_oidc_issuer_arn : try(each.value.cluster_identity_oidc_issuer_arn, null)
 
   irsa_role_create      = var.irsa_role_create != null ? var.irsa_role_create : try(each.value.irsa_role_create, null)
-  irsa_role_name_prefix = var.irsa_role_name_prefix != null ? var.irsa_role_name_prefix : try(each.value.irsa_role_name_prefix, "${each.key}-irsa")
+  irsa_role_name_prefix = var.irsa_role_name_prefix != null ? var.irsa_role_name_prefix : try(each.value.irsa_role_name_prefix, "${local.addon.name}-irsa")
   irsa_role_name        = var.irsa_role_name != null ? var.irsa_role_name : try(each.value.irsa_role_name, local.addon_name)
 
   irsa_policy_enabled       = var.irsa_policy_enabled != null ? var.irsa_policy_enabled : try(each.value.irsa_policy_enabled, null)
@@ -35,7 +35,7 @@ module "addon-irsa" {
   cluster_name = var.cluster_name != null ? var.cluster_name : try(each.value.cluster_name, null)
 
   pod_identity_role_create      = var.pod_identity_role_create != null ? var.pod_identity_role_create : try(each.value.pod_identity_role_create, null)
-  pod_identity_role_name_prefix = var.pod_identity_role_name_prefix != null ? var.pod_identity_role_name_prefix : try(each.value.pod_identity_role_name_prefix, "${each.key}-pi")
+  pod_identity_role_name_prefix = var.pod_identity_role_name_prefix != null ? var.pod_identity_role_name_prefix : try(each.value.pod_identity_role_name_prefix, "${local.addon.name}-pi")
   pod_identity_role_name        = var.pod_identity_role_name != null ? var.pod_identity_role_name : try(each.value.pod_identity_role_name, local.addon_name)
 
   pod_identity_policy_enabled       = var.pod_identity_policy_enabled != null ? var.pod_identity_policy_enabled : try(each.value.pod_identity_policy_enabled, null)
@@ -47,6 +47,6 @@ module "addon-irsa" {
 }
 
 output "addon_irsa" {
-  description = "The addon IRSA module outputs"
+  description = "The IRSA addon module outputs"
   value       = module.addon-irsa
 }
