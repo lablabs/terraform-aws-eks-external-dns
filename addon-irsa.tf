e3f5a919a87a48cf56e8d7027c453d0eea00224f
atyu1 <github-action@actions-template-sync.noreply.github.com>
2025-06-26 00:29:47 +0000

    feat(sync): sync universal-addon changes
    Signed-off-by: atyu1 <github-action@actions-template-sync.noreply.github.com>

--- addon-irsa.tf
+++ addon-irsa.tf
@@ -2,48 +2,48 @@
 module "addon-irsa" {
   for_each = local.addon_irsa
 
-  source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon-irsa?ref=v0.0.21"
+  source = "git::https://github.com/lablabs/terraform-aws-eks-universal-addon.git//modules/addon-irsa?ref=v0.0.22"
 
   enabled = var.enabled
 
-  rbac_create               = var.rbac_create != null ? var.rbac_create : try(each.value.rbac_create, null)
-  service_account_create    = var.service_account_create != null ? var.service_account_create : try(each.value.service_account_create, null)
-  service_account_name      = var.service_account_name != null ? var.service_account_name : try(each.value.service_account_name, local.addon_name)
-  service_account_namespace = var.service_account_namespace != null ? var.service_account_namespace : try(each.value.service_account_namespace, local.addon_namespace)
+  rbac_create               = var.rbac_create != null ? var.rbac_create : lookup(local.addon, "rbac_create", null)
+  service_account_create    = var.service_account_create != null ? var.service_account_create : lookup(local.addon, "service_account_create", null)
+  service_account_name      = var.service_account_name != null ? var.service_account_name : lookup(local.addon, "service_account_name", local.addon_name)
+  service_account_namespace = var.service_account_namespace != null ? var.service_account_namespace : lookup(local.addon, "service_account_namespace", local.addon_namespace)
 
   # IRSA
-  cluster_identity_oidc_issuer     = var.cluster_identity_oidc_issuer != null ? var.cluster_identity_oidc_issuer : try(each.value.cluster_identity_oidc_issuer, null)
-  cluster_identity_oidc_issuer_arn = var.cluster_identity_oidc_issuer_arn != null ? var.cluster_identity_oidc_issuer_arn : try(each.value.cluster_identity_oidc_issuer_arn, null)
+  cluster_identity_oidc_issuer     = var.cluster_identity_oidc_issuer != null ? var.cluster_identity_oidc_issuer : lookup(local.addon, "cluster_identity_oidc_issuer", null)
+  cluster_identity_oidc_issuer_arn = var.cluster_identity_oidc_issuer_arn != null ? var.cluster_identity_oidc_issuer_arn : lookup(local.addon, "cluster_identity_oidc_issuer_arn", null)
 
-  irsa_role_create      = var.irsa_role_create != null ? var.irsa_role_create : try(each.value.irsa_role_create, null)
-  irsa_role_name_prefix = var.irsa_role_name_prefix != null ? var.irsa_role_name_prefix : try(each.value.irsa_role_name_prefix, "${local.addon.name}-irsa")
-  irsa_role_name        = var.irsa_role_name != null ? var.irsa_role_name : try(each.value.irsa_role_name, local.addon_name)
+  irsa_role_create      = var.irsa_role_create != null ? var.irsa_role_create : lookup(local.addon, "irsa_role_create", null)
+  irsa_role_name_prefix = var.irsa_role_name_prefix != null ? var.irsa_role_name_prefix : lookup(local.addon, "irsa_role_name_prefix", "${local.addon.name}-irsa")
+  irsa_role_name        = var.irsa_role_name != null ? var.irsa_role_name : lookup(local.addon, "irsa_role_name", local.addon_name)
 
-  irsa_policy_enabled       = var.irsa_policy_enabled != null ? var.irsa_policy_enabled : try(each.value.irsa_policy_enabled, null)
-  irsa_policy               = var.irsa_policy != null ? var.irsa_policy : try(each.value.irsa_policy, null)
-  irsa_assume_role_enabled  = var.irsa_assume_role_enabled != null ? var.irsa_assume_role_enabled : try(each.value.irsa_assume_role_enabled, null)
-  irsa_assume_role_arns     = var.irsa_assume_role_arns != null ? var.irsa_assume_role_arns : try(each.value.irsa_assume_role_arns, null)
-  irsa_permissions_boundary = var.irsa_permissions_boundary != null ? var.irsa_permissions_boundary : try(each.value.irsa_permissions_boundary, null)
-  irsa_additional_policies  = var.irsa_additional_policies != null ? var.irsa_additional_policies : lookup(each.value, "irsa_additional_policies", null)
+  irsa_policy_enabled       = var.irsa_policy_enabled != null ? var.irsa_policy_enabled : lookup(local.addon, "irsa_policy_enabled", null)
+  irsa_policy               = var.irsa_policy != null ? var.irsa_policy : lookup(local.addon, "irsa_policy", null)
+  irsa_assume_role_enabled  = var.irsa_assume_role_enabled != null ? var.irsa_assume_role_enabled : lookup(local.addon, "irsa_assume_role_enabled", null)
+  irsa_assume_role_arns     = var.irsa_assume_role_arns != null ? var.irsa_assume_role_arns : lookup(local.addon, "irsa_assume_role_arns", null)
+  irsa_permissions_boundary = var.irsa_permissions_boundary != null ? var.irsa_permissions_boundary : lookup(local.addon, "irsa_permissions_boundary", null)
+  irsa_additional_policies  = var.irsa_additional_policies != null ? var.irsa_additional_policies : lookup(local.addon, "irsa_additional_policies", null)
 
-  irsa_assume_role_policy_condition_test   = var.irsa_assume_role_policy_condition_test != null ? var.irsa_assume_role_policy_condition_test : try(each.value.irsa_assume_role_policy_condition_test, null)
-  irsa_assume_role_policy_condition_values = var.irsa_assume_role_policy_condition_values != null ? var.irsa_assume_role_policy_condition_values : try(each.value.irsa_assume_role_policy_condition_values, null)
+  irsa_assume_role_policy_condition_test   = var.irsa_assume_role_policy_condition_test != null ? var.irsa_assume_role_policy_condition_test : lookup(local.addon, "irsa_assume_role_policy_condition_test", null)
+  irsa_assume_role_policy_condition_values = var.irsa_assume_role_policy_condition_values != null ? var.irsa_assume_role_policy_condition_values : lookup(local.addon, "irsa_assume_role_policy_condition_values", null)
 
-  irsa_tags = var.irsa_tags != null ? var.irsa_tags : try(each.value.irsa_tags, null)
+  irsa_tags = var.irsa_tags != null ? var.irsa_tags : lookup(local.addon, "irsa_tags", null)
 
   # Pod identity
-  cluster_name = var.cluster_name != null ? var.cluster_name : try(each.value.cluster_name, null)
+  cluster_name = var.cluster_name != null ? var.cluster_name : lookup(local.addon, "cluster_name", null)
 
-  pod_identity_role_create      = var.pod_identity_role_create != null ? var.pod_identity_role_create : try(each.value.pod_identity_role_create, null)
-  pod_identity_role_name_prefix = var.pod_identity_role_name_prefix != null ? var.pod_identity_role_name_prefix : try(each.value.pod_identity_role_name_prefix, "${local.addon.name}-pi")
-  pod_identity_role_name        = var.pod_identity_role_name != null ? var.pod_identity_role_name : try(each.value.pod_identity_role_name, local.addon_name)
+  pod_identity_role_create      = var.pod_identity_role_create != null ? var.pod_identity_role_create : lookup(local.addon, "pod_identity_role_create", null)
+  pod_identity_role_name_prefix = var.pod_identity_role_name_prefix != null ? var.pod_identity_role_name_prefix : lookup(local.addon, "pod_identity_role_name_prefix", "${local.addon.name}-pi")
+  pod_identity_role_name        = var.pod_identity_role_name != null ? var.pod_identity_role_name : lookup(local.addon, "pod_identity_role_name", local.addon_name)
 
-  pod_identity_policy_enabled       = var.pod_identity_policy_enabled != null ? var.pod_identity_policy_enabled : try(each.value.pod_identity_policy_enabled, null)
-  pod_identity_policy               = var.pod_identity_policy != null ? var.pod_identity_policy : try(each.value.pod_identity_policy, null)
-  pod_identity_permissions_boundary = var.pod_identity_permissions_boundary != null ? var.pod_identity_permissions_boundary : try(each.value.pod_identity_permissions_boundary, null)
-  pod_identity_additional_policies  = var.pod_identity_additional_policies != null ? var.pod_identity_additional_policies : lookup(each.value, "pod_identity_additional_policies", null)
+  pod_identity_policy_enabled       = var.pod_identity_policy_enabled != null ? var.pod_identity_policy_enabled : lookup(local.addon, "pod_identity_policy_enabled", null)
+  pod_identity_policy               = var.pod_identity_policy != null ? var.pod_identity_policy : lookup(local.addon, "pod_identity_policy", null)
+  pod_identity_permissions_boundary = var.pod_identity_permissions_boundary != null ? var.pod_identity_permissions_boundary : lookup(local.addon, "pod_identity_permissions_boundary", null)
+  pod_identity_additional_policies  = var.pod_identity_additional_policies != null ? var.pod_identity_additional_policies : lookup(local.addon, "pod_identity_additional_policies", null)
 
-  pod_identity_tags = var.pod_identity_tags != null ? var.pod_identity_tags : try(each.value.pod_identity_tags, null)
+  pod_identity_tags = var.pod_identity_tags != null ? var.pod_identity_tags : lookup(local.addon, "pod_identity_tags", null)
 }
 
 output "addon_irsa" {
--- examples/basic/versions.tf
+++ examples/basic/versions.tf
@@ -8,11 +8,11 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">= 2.20.0"
+      version = "~> 2.20"
     }
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.6.0"
+      version = "~> 2.6"
     }
   }
 }
